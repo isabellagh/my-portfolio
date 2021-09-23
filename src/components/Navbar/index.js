85edba2ac6bb977ff75965bdcf16ddebe0363772
isabellagh <isabellagh88@gmail.com>
2021-09-23 01:10:25 -0400

    about me and hamburger icon navbar styled

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,15 +1,22 @@
 import React from "react";
-import { Nav, NavbarContainer, NavLogo } from "./NavbarElements";
+import { FaBars } from 'react-icons/fa'
+import { Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks } from "./NavbarElements";
 
 const Navbar = () => {
   return (
     <>
       <Nav>
-      <NavbarContainer>
-          <NavLogo to='/'>
-              <h1>Bella</h1>
-          </NavLogo>
-      </NavbarContainer>
+        <NavbarContainer>
+          <NavLogo to="/">Bella</NavLogo>
+          <MobileIcon>
+              <FaBars />
+          </MobileIcon>
+          <NavMenu>
+              <NavItem>
+                  <NavLinks to='about-me'>About Me</NavLinks>
+              </NavItem>
+          </NavMenu>
+        </NavbarContainer>
       </Nav>
     </>
   );
