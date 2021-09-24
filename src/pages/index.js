ae5c5c2eb3d579a4941d617dc3ffc84beab1d344
isabellagh <isabellagh88@gmail.com>
2021-09-24 14:30:43 -0400

    mobile sidebar + styling

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React, {useState} from "react";
+import SideBar from "../components/SideBar";
+import Navbar from "../components/Navbar";
+
+const Home = () => {
+  return (
+    <>
+      <SideBar />
+      <Navbar />
+    </>
+  );
+};
+
+export default Home;
