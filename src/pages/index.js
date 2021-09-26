320aa655013c01f4fdbeb63745781c4b35ab7c35
isabellagh <isabellagh88@gmail.com>
2021-09-25 22:50:14 -0400

    section folders created

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,10 @@ import React, {useState} from "react";
 import SideBar from "../components/SideBar";
 import Navbar from "../components/Navbar";
 import About from "../components/about";
+import Projects from "../components/projects"
+import Skills from "../components/skills"
+import Experience from "../components/experience"
+import Contact from "../components/contact"
 
 const Home = () => {
     const [isOpen, setIsOpen] = useState(false)
@@ -15,6 +19,10 @@ const Home = () => {
       <SideBar isOpen={isOpen} toggle={toggle} />
       <Navbar toggle={toggle}/>
       <About />
+      <Projects />
+      <Skills />
+      <Experience />
+      <Contact />
     </>
   );
 };
