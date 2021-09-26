0081d33d9c43f8ee200674f7a958bf3a30e9dcf7
isabellagh <isabellagh88@gmail.com>
2021-09-25 23:34:08 -0400

    ProjectElement added to Projects

--- src/components/projects/index.js
+++ src/components/projects/index.js
@@ -1,11 +1,41 @@
-import React from 'react'
+import React from "react";
+import {
+  ProjectContainer,
+  ProjectWrapper,
+  ProjectRow,
+  Column1,
+  TextWrapper,
+  TopLine,
+  Heading,
+  Subtitle,
+  Column2,
+  ImgWrap,
+  Img,
+} from "../projects/ProjectElements";
 
 const Projects = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <>
+      <ProjectContainer>
+        <ProjectWrapper>
+          <ProjectRow>
+            <Column1>
+              <TextWrapper>
+                <TopLine>TopLine</TopLine>
+                <Heading>Heading</Heading>
+                <Subtitle>Subtitle</Subtitle>
+              </TextWrapper>
+            </Column1>
+            <Column2>
+              <ImgWrap>
+                <Img />
+              </ImgWrap>
+            </Column2>
+          </ProjectRow>
+        </ProjectWrapper>
+      </ProjectContainer>
+    </>
+  );
+};
 
-export default Projects
+export default Projects;
