dabbe28387da4280d8b5f94d39675275c61fde4b
isabellagh <isabellagh88@gmail.com>
2021-09-25 23:51:30 -0400

    datafile

---
+++ src/components/projects/Data.js
@@ -0,0 +1,16 @@
+export const homeObjOne = {
+    // id: 'projects',
+    // lightBg: false,
+    // lightText: true,
+    // lightTextDesc: true,
+    // // topLine: '',
+    // headLine: 'Soccer Star',
+    // description: 'A place where you can add your favorite soccer players and rate them.',
+    // buttomLabel: 'See more',
+    // imgStart: false,
+    // img: "https://images.pexels.com/photos/5894435/pexels-photo-5894435.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    // alt: 'Soccer',
+    // dark: true,
+    // primary: true,
+    // darkText: false,
+}
