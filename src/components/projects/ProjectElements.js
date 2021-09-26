16fe00a4b6eedaf029d46420ac29b908170cf415
isabellagh <isabellagh88@gmail.com>
2021-09-26 13:57:13 -0400

    projects card

--- src/components/projects/ProjectElements.js
+++ src/components/projects/ProjectElements.js
@@ -1,97 +1,85 @@
 import styled from "styled-components";
 
 export const ProjectContainer = styled.div`
-  color: #fff;
-  background: ${({lightBg}) => (lightBg ? '#f9f9f9' : '010606')};
+  height: 800px;
+  display: flex;
+  flex-direction:column;
+  justufy-content: center;
+  align-items: center;
+  background: #010606;
 
   @media screen and (max-width: 768px) {
-      padding: 100px 0;
+      height: 1100px;
+  }
+  @media screen and (max-width: 480px) {
+      height: 1300px;
   }
 `
 
 export const ProjectWrapper = styled.div`
+  max-width: 1000px;
+  margin: 0 auto;
   display: grid;
-  z-index: 1;
-  height: 860px;
-  width: 100px;
-  max-width: 1100px;
-  margin-right: auto;
-  margin-left: auto;
-  padding: 0 24px;
-  justify-content: center;
-`
-
-export const ProjectRow = styled.div`
-  display: grid;
-  grid-auto-columns: minmax(auto, 1fr);
+  grid-template-columns: 1fr 1fr 1fr;
   align-items: center;
-  grid-template-areas: ${({imgStart}) => (imgStart ? `'col2 col1'` : `'col1 col2'` )};
-
-  @media screen and (max-width: 768px) {
-      grid-template-areas: ${({imgStart}) => (imgStart ? `'col1' 'col2'` : `'col1 col1' 'col2 col2'`)}
+  grid-gap: 16px;
+  padding: 0 50px;
+  
+  @media screen and (max-width: 1000px) {
+    grid-template-columns: 1fr 1fr;
+  }
+  @media screen and (max-width: 1000px) {
+    grid-template-columns: 1fr;
+    padding: 0 20px;
   }
 `
 
-export const Column1 = styled.div`
-  margin-bottom: 15px;
-  padding: 0 15px;
-  grid-area: col1;
-`
-
-export const Column2 = styled.div`
-  margin-bottom: 15px;
-  padding: 0 15px;
-  grid-area: col2;
-`
-
-export const TextWrapper = styled.div`
-  max-width: 540px;
-  padding-top: 0;
-  padding-bottom: 60px
-`
-
-export const TopLine = styled.p`
-  color: #01bf71;
-  font-size: 16px;
-  line-height: 16px;
-  font-weight: 700;
-  letter-spacing: 1.4px;
-  text-transform: uppercase;
-  margin-bottom: 16px;
-`
-
-export const Heading =styled.h1`
-  margin-bottom: 24px;
-  font-size: 48px;
-  line-height: 1.1;
-  font-weight: 600;
-  color: ${({ lightText }) => (lightText ? '#f7f8fa' : '010606')};
-
-  @media screen and (max-width: 480px) {
-      font-size: 32px;
-  }
-`
-
-export const Subtitle = styled.p`
-  max-width: 440px;
-  margin-bottom: 35px;
-  font-size: 18px;
-  line-height: 24px;
-  color: ${({darkText}) => (darkText ? '#010606' : '#fff')};
-`
-
-export const BtnWrap = styled.div`
+export const ProjectCard = styled.div`
+  background: #fff;
   display: flex;
+  flex-direction: column;
   justify-content: flex-start;
+  align-items: center;
+  border-radius: 10px;
+  max-height: 340px;
+  padding: 30px;
+  box-shadow: 0 1px 3px rgba(0,0,0,0.2);
+  transition: all 0.2s ease-in-out;
+
+  &:hover {
+      transform: scale(1.02);
+      transition: all 0.2s ease-in-out;
+      cursor: pointer;
+  }
 `
 
-export const ImgWrap = styled.div`
-  max-width: 555px;
-  height: 100%
+export const ProjectsImg = styled.img`
+  height: 160px;
+  width: 160px;
+  margin-bottom: 10px;
 `
 
-export const Img = styled.img`
-  width: 100%;
-  margin: 0 0 10px 0;
-  padding-right: 0;
+export const ProjectsH1 = styled.h1`
+  font-size: 2.5rem;
+  color: #fff;
+  margin-bottom: 64px;
+  
+  @media screen and (max-width: 480px) {
+      font-size: 2rem;
+  }
 `
+
+export const ProjectsH2 = styled.h1`
+  font-size: 1rem;
+  margin-bottom: 10px;
+`
+
+export const ProjectsP = styled.p`
+  font-size: 1rem;
+  text-align: center;
+`
+
+// export const BtnWrap = styled.div`
+//   display: flex;
+//   justify-content: flex-start;
+// `
--- src/components/projects/index.js
+++ src/components/projects/index.js
@@ -1,37 +1,44 @@
 import React from "react";
 import {
   ProjectContainer,
+  ProjectsH1,
   ProjectWrapper,
-  ProjectRow,
-  Column1,
-  TextWrapper,
-  TopLine,
-  Heading,
-  Subtitle,
-  Column2,
-  ImgWrap,
-  Img,
+  ProjectCard,
+  ProjectsImg,
+  ProjectsH2,
+  ProjectsP,
 } from "../projects/ProjectElements";
 
 const Projects = () => {
   return (
     <>
-      <ProjectContainer>
+      <ProjectContainer id="projects">
+        <ProjectsH1>Projects</ProjectsH1>
         <ProjectWrapper>
-          <ProjectRow>
-            <Column1>
-              <TextWrapper>
-                <TopLine>TopLine</TopLine>
-                <Heading>Heading</Heading>
-                <Subtitle>Subtitle</Subtitle>
-              </TextWrapper>
-            </Column1>
-            <Column2>
-              <ImgWrap>
-                <Img />
-              </ImgWrap>
-            </Column2>
-          </ProjectRow>
+          <ProjectCard>
+            <ProjectsImg src="https://images.pexels.com/photos/5894435/pexels-photo-5894435.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+            <ProjectsH2>Soccer Star</ProjectsH2>
+            <ProjectsP>
+              "A place where you can add your favorite soccer players and rate
+              them. "
+            </ProjectsP>
+          </ProjectCard>
+          <ProjectCard>
+            <ProjectsImg src="https://images.pexels.com/photos/5894435/pexels-photo-5894435.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+            <ProjectsH2>Soccer Star</ProjectsH2>
+            <ProjectsP>
+              "A place where you can add your favorite soccer players and rate
+              them. "
+            </ProjectsP>
+          </ProjectCard>
+          <ProjectCard>
+            <ProjectsImg src="https://images.pexels.com/photos/5894435/pexels-photo-5894435.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+            <ProjectsH2>Soccer Star</ProjectsH2>
+            <ProjectsP>
+              "A place where you can add your favorite soccer players and rate
+              them. "
+            </ProjectsP>
+          </ProjectCard>
         </ProjectWrapper>
       </ProjectContainer>
     </>
