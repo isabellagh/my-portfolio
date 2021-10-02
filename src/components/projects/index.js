1af2de57da53f9c55601437a09813138bd033c2d
isabellagh <isabellagh88@gmail.com>
2021-10-02 11:58:23 -0400

    footer boilerplate

--- src/components/projects/index.js
+++ src/components/projects/index.js
@@ -16,25 +16,36 @@ const Projects = () => {
         <ProjectsH1>Projects</ProjectsH1>
         <ProjectWrapper>
           <ProjectCard>
-            <ProjectsImg src={require("../../../src/images/soccer.png").default} />
+            <ProjectsImg
+              src={require("../../../src/images/soccer.png").default}
+            />
             <ProjectsH2>Soccer Star</ProjectsH2>
             <ProjectsP>
-              A place where you can add your favorite soccer players and rate
-              them.
+              <p>
+                A place where you can add your favorite soccer players and rate
+                them.
+              </p>
             </ProjectsP>
           </ProjectCard>
           <ProjectCard>
-            <ProjectsImg src={require("../../../src/images/kids.png").default} />
+            <ProjectsImg
+              src={require("../../../src/images/kids.png").default}
+            />
             <ProjectsH2>TCC</ProjectsH2>
             <ProjectsP>
-            A daycare data organizer.
+              <p>A daycare data organizer.</p>
             </ProjectsP>
           </ProjectCard>
           <ProjectCard>
-            <ProjectsImg src={require("../../../src/images/personal-trainer.png").default} />
+            <ProjectsImg
+              src={require("../../../src/images/personal-trainer.png").default}
+            />
             <ProjectsH2>PT Space</ProjectsH2>
             <ProjectsP>
-              A place where Personal Traniers can keep track of their clients and also people can find and contact trainers.
+              <p>
+                A place where Personal Traniers can keep track of their clients
+                and also people can find and contact trainers.
+              </p>
             </ProjectsP>
           </ProjectCard>
         </ProjectWrapper>
@@ -42,6 +53,5 @@ const Projects = () => {
     </>
   );
 };
-
 
 export default Projects;
