e078aac615df2f58322a9c09bc52ff3566980936
isabellagh <isabellagh88@gmail.com>
2021-10-10 14:21:06 -0400

    footer extra css deleted

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,34 +1,11 @@
 import React from "react";
-import {
-  FooterContainer,
-  FooterWrapper,
-  // FooterLinksContainer,
-  // FooterLinksWrapper,
-  // FooterLinkItems,
-     FooterTitle,
-  // FooterLink,
-} from "./FooterElements";
+import { FooterContainer, FooterWrapper, FooterTitle } from "./FooterElements";
 
 const Footer = () => {
   return (
     <FooterContainer>
       <FooterWrapper>
-      <FooterTitle> © 2021 Isabella Henriques</FooterTitle>
-        
-        {/* <FooterLinksContainer>
-          <FooterLinksWrapper>
-            <FooterLinkItems>
-              <FooterLinkTitle> © Isabella Henriques</FooterLinkTitle>
-              <FooterLink></FooterLink>
-              <FooterLink></FooterLink>Ï
-            </FooterLinkItems>
-            <FooterLinkItems>
-              <FooterLinkTitle></FooterLinkTitle>
-              <FooterLink></FooterLink>
-              <FooterLink></FooterLink>Ï
-            </FooterLinkItems>
-          </FooterLinksWrapper>
-        </FooterLinksContainer> */}
+        <FooterTitle> © 2021 Isabella Henriques</FooterTitle>
       </FooterWrapper>
     </FooterContainer>
   );
